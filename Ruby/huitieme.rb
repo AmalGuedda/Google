9484c282b8ac8b9215394f1f19fca695cf78c13e
AmalGuedda <82024475+AmalGuedda@users.noreply.github.com>
2021-04-13 18:58:29 +0200

    Add files via upload

---
+++ Ruby/huitieme.rb
@@ -0,0 +1,6 @@
+puts "entrez un nombre"
+i = gets.chomp.to_i
+while i != 0
+    i = i + -1
+    puts i
+end
---
+++ Ruby/neuvieme.rb
@@ -0,0 +1,6 @@
+puts "quelle est votre annee de naissance"
+i = gets.chomp.to_i
+while i != 2021
+    i = i + 1
+    puts i
+end
